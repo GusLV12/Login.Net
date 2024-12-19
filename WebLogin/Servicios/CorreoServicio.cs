1ba5369d2c5915299687f552b0d9464457af2b77
Gustavo Linares Villegas <guslinaresv@gmail.com>
2024-12-18 21:19:37 -0600

    Add: new views recovery

--- WebLogin/Servicios/CorreoServicio.cs
+++ WebLogin/Servicios/CorreoServicio.cs
@@ -12,6 +12,35 @@ namespace WebLogin.Servicios
         private static int _Puerto = 587;
         private static string _NombreSend = "GusLV";
         private static string _Correo = "gusmedia117@gmail.com";
-        private static string _Cllave = "";
+        private static string _Clave = "udimojgjvxfprbqc";
+        public static bool Enviar(CorreoDTO correodto)
+        {
+            try
+            {
+                var email = new MimeMessage();
+
+                email.From.Add(new MailboxAddress(_NombreSend, _Correo));
+                email.To.Add(MailboxAddress.Parse(correodto.Para));
+                email.Subject = correodto.Asunto;
+                email.Body = new TextPart(TextFormat.Html)
+                {
+                    Text = correodto.Contenido
+                };
+
+                var smtp = new SmtpClient();
+                smtp.Connect(_Host, _Puerto, SecureSocketOptions.StartTls);
+
+                smtp.Authenticate(_Correo, _Clave);
+                smtp.Send(email);
+                smtp.Disconnected();
+
+               return true;
+
+            }catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
+
 }
